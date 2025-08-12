a93ccaf30b67febc310fce30966b36424e5ae107
Anushka Sah <sahanushka1820@gmail.com>
2025-08-12 13:24:56 +0530

    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getOrderItems } from '../controllers/orderController.js';
-
-const router = express.Router();
-
-router.get('/:orderId/items', getOrderItems);  // GET /api/orders/:orderId/items
-
-export default router;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsers, getUserOrders } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/', getUsers);                // GET /api/users?search=...
-router.get('/:userId/orders', getUserOrders); // GET /api/users/:userId/orders
-
-export default router;
